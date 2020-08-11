2c6fe319a49d6759a637aa2a743b48cbb71ba011
nikita1469 <pogrebnoi.nikita2505@gmail.com>
2020-08-11 13:23:47 +0300

    slider

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import modules from './modules/modals';
+import modals from './modules/modals';
+import sliders from './modules/sliders';
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
+});
