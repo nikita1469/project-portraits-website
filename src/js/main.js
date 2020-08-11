aec1fc7678c372fef6530291bc33b8b7022b4fae
nikita1469 <pogrebnoi.nikita2505@gmail.com>
2020-08-11 20:12:52 +0300

    validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-import modules from './modules/modals';
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -8,4 +10,8 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
